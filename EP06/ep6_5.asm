4de53d0174b1e5a3ff86e66d94bc9821ef39c855
Arthur de Sá Braz de Matos <129903540+arthursbmatos9@users.noreply.github.com>
2024-05-11 20:01:28 -0300

    Update ep6_5.asm

--- EP06/ep6_5.asm
+++ EP06/ep6_5.asm
@@ -3,9 +3,9 @@
 
 .text
 ori $t0, $zero, 0x186A
-sll $s0, $t0, 1 #x = 100.000
+sll $s0, $t0, 4 #x = 100.000
 
 ori $t0, $zero, 0x30D4
-sll $s1, $t0, 1 #y = 200.000
+sll $s1, $t0, 4 #y = 200.000
 
 add $s2, $s1, $s0 #z = x + y
