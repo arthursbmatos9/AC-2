d27f897eec57bea30a2f735da4723d5f484b4ae7
Arthur de Sá Braz de Matos <129903540+arthursbmatos9@users.noreply.github.com>
2024-05-11 20:01:52 -0300

    Add files via upload

---
+++ EP06/ep6_6.asm
@@ -0,0 +1,15 @@
+#x = $s0
+#y = $s1
+#z = $s2
+
+.text
+ori $t0, $zero, 0xFFFF
+sll $t0, $t0, 16  #deslocando para a esquerda, ficando 0xFFFF0000
+ori $s0, $t0, 0xFFFF #x = FFFFFFFF
+
+ori $t0, $zero, 0x493E 
+sll $s1, $t0, 4 #y = 300.000
+
+sll $t0, $s1, 16 #t0 = 4y
+
+sub $s2, $s0, $t0 #z = x - 4y
